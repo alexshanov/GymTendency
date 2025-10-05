1c79709674e379c5539baa131a2db464aff4e6db
Alex Shanov <alexander.shanov@gmail.com>
2025-10-05 15:45:37 -0600

    kscore scraper debug mode off

--- kscore_scraper.py
+++ kscore_scraper.py
@@ -17,7 +17,7 @@ from selenium.webdriver.support import expected_conditions as EC
 # --- CONFIGURATION ---
 KSCORE_MEETS_CSV = "discovered_meet_ids_kscore.csv"
 OUTPUT_DIR_KSCORE = "CSVs_final_kscore"
-DEBUG_LIMIT = 1
+DEBUG_LIMIT = 0
 
 # ==============================================================================
 #  THE PROVEN, WORKING HELPER FUNCTION
